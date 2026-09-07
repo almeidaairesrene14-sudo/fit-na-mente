3090cde352efcaa7a850c1938d3d2356328efe7b
almeidaairesrene14-sudo <almeidaairesrene14@gmail.com>
2026-09-07 13:25:54 -0400

    Delete templates.js

--- templates.js
+++
@@ -1,5 +0,0 @@
-const TEMPLATES = [
-  {id:"fit", name:"Fit Clean", top:"rgba(0,0,0,.28)", bottom:"rgba(0,0,0,.18)", border:0},
-  {id:"dark", name:"Dark Bars", top:"rgba(0,0,0,.62)", bottom:"rgba(0,0,0,.58)", border:0},
-  {id:"frame", name:"Frame", top:"rgba(0,0,0,.18)", bottom:"rgba(0,0,0,.18)", border:18}
-];
